fc8ba513033400c60d45d19dae29c21c2ecd9e4c
vickyguan3 <42555135+vickyguan3@users.noreply.github.com>
2018-10-30 13:18:54 -0700

    Update Farm.java

--- Farm.java
+++ Farm.java
@@ -1,4 +1,19 @@
-class Farm 
-{     
-   //your code here
+class Farm
+{
+  private Animal[] aBunchOfAnimals = new Animal[3];
+  public Farm()
+  {
+    aBunchOfAnimals[0] = new NamedCow("cow", "Curry", "moooo");
+    aBunchOfAnimals[1] = new Chick("chick", "chirp", "jeep");
+    aBunchOfAnimals[2] = new Pig("pig", "oink oink");
+  }
+
+  public void animalSounds()
+    {       
+      for (int nI=0; nI < aBunchOfAnimals.length; nI++)       
+      {          
+        System.out.println( aBunchOfAnimals[nI].getType() + " goes " + aBunchOfAnimals[nI].getSound());
+      } 
+      System.out.println("The cow is known as " + (((NamedCow)aBunchOfAnimals[0]).getName()));
+    }
 }
